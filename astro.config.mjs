95ae4e3eea403c152c93d4a0cbd20448908858e6
Sonny Mattera <smattera@hotmail.com>
2025-04-25 00:41:24 -0500

    feat: Update site and base configuration in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,8 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://smattera.github.io",
+  base: "astro-app",
   vite: {
     plugins: [tailwindcss()],
   },
